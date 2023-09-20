6ee1a662f933b154aea9d4265e117545e1181110
Sudha Bucks <sudhabucks.657@outlook.com>
2023-09-20 10:53:36 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,15 @@ terraform {
       version = "~> 2.0"
     }
   }
-
-}
-  
-terraform {
   backend "azurerm" {}
 }
-  
+
+provider "azurerm" {
+  features {}
+}
+
 data "azurerm_client_config" "current" {}
-  
+
 resource "azurerm_resource_group" "tamopsrg" {
   name     = "tamops-tf"
   location = "uksouth"
