5b23087d857651f9199c16980400eff0787e6529
Sudha Bucks <sudhabucks.657@outlook.com>
2023-09-20 10:15:14 +0000

    Added main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+  
+terraform {
+  backend "azurerm" {}
+}
+  
+data "azurerm_client_config" "current" {}
+  
+resource "azurerm_resource_group" "tamopsrg" {
+  name     = "tamops-tf"
+  location = "uksouth"
+}
