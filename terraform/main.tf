6bfbb374a94eaf59bf2cd51b65539befca1c990d
Sudha Bucks <sudhabucks.657@outlook.com>
2023-09-20 10:51:32 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,11 @@
-provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x.
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
-    features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+
 }
   
 terraform {
